fc665396033672749bb54d47757627c4a096a0e3
Alexei <alexei@alexey.com>
2023-03-22 11:47:43 +0300

    task2

--- task1.java
+++ task1.java
@@ -1,4 +1,9 @@
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 // Реализуйте алгоритм сортировки пузырьком числового массива (введён вами),
 // результат после каждой итерации запишите в лог-файл.
@@ -8,8 +13,17 @@ import java.util.Arrays;
  */
 public class task1 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
+        Logger ll = Logger.getLogger(task1.class.getName());
+        String logsPath = "log.txt";
+        FileHandler fh = new FileHandler(logsPath, false);
+        ll.addHandler(fh);
+        SimpleFormatter formatter = new SimpleFormatter();
+        // XMLFormatter form = new XMLFormatter();
+        fh.setFormatter(formatter);
+    
         int[] array = { 2, 69, 32, 0, 17, 89, 42, 5, 9, 11 };
+        ll.log(Level.INFO,Arrays.toString(array));
         System.out.println(Arrays.toString(array));
         int temp;
         for (int i = 0; i < array.length - 1; i++) {
@@ -20,7 +34,7 @@ public class task1 {
                     array[j + 1] = temp;
                 }
             }
-            System.out.println(Arrays.toString(array));
+            ll.log(Level.INFO,Arrays.toString(array));
         }
     }
 }
